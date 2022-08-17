aec4c89e91b9bd2678c898c66f46583c7666355e
Jumoke Boladale-Lawal <76624942+jutivia@users.noreply.github.com>
2022-08-17 23:33:03 +0100

    removing unused routes from the app file

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express()
 const connectDb = require('./db/connect')
-const authRoute = require('./routes/auth')
-const publicationRoute = require('./routes/publication')
+// const authRoute = require('./routes/auth')
+// const publicationRoute = require('./routes/publication')
 const notFound = require('./middleware/not-found')
 const errorHandler = require("./middleware/error-handler");
 const auth = require("./middleware/authentication");
@@ -30,8 +30,8 @@ app.use(xss());
 
 
 app.use(express.json());
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/publications", [auth, publicationRoute])
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/publications", [auth, publicationRoute])
 
 app.use(notFound, errorHandler);
 
