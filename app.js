08f291da19208eafbe3bdb5148bfc3c27344d308
Jumoke Boladale-Lawal <76624942+jutivia@users.noreply.github.com>
2022-08-24 00:43:32 +0100

    m

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
 
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const start = async() => {
     try {
         await connectDb(process.env.MONGO_URI)
